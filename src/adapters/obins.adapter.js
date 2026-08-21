916fd7d7715bcfdf2924a74157198e2547e5e638
potapovroman698-afk <potapovroman698@gmail.com>
2026-08-21 18:52:14 +0200

    feat: add safe Obins adapter boundary

---
+++ src/adapters/obins.adapter.js
@@ -0,0 +1,7 @@
+export class ObinsAdapter {
+  constructor() {
+    throw new Error(
+      'Obins integration is not implemented. Add it only after official API details are verified.'
+    );
+  }
+}
